f0a2ed3c86d3e645951fd9b10713dffd67006fcf
Ogbuagu Bribget <b.ogbuagu@alustudent.com>
2023-11-09 01:18:29 -0800

    my first commmit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx > len(my_list) - 1:
-        return my_list
-    my_list[idx] = element
-    return my_list
+def safe_print_list_integers(my_list=[], x=0):
+    funct = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            funct += 1
+    print()
+    return (funct)
