c208fb80bbcdc85a746af568a291dbf1f55960df
Ogbuagu Bribget <b.ogbuagu@alustudent.com>
2023-11-09 01:15:23 -0800

    my first commmit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
+    my_list[idx] = element
+    return my_list
