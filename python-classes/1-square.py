7c37ad0a27b363f4bacd2ba462d629f7f833a38a
Ogbuagu Bribget <b.ogbuagu@alustudent.com>
2023-11-14 01:56:56 -0800

    my first commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""
+Write a class Square that defines a square
+with private fields
+"""
+
+
+class Square:
+    """
+    This class takes creates a square
+    """
+
+    def __init__(self, size):
+        """
+        Pass in the size of the square
+        """
+        self.__size = size
