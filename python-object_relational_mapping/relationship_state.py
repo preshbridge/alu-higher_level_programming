a7824bed95eb88f90ae6694a990bef1816250c88
preshbridge <b.ogbuagu@alustudent.com>
2023-12-14 08:04:21 -0800

    m

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+class definition of a State and an instance Base = declarative_base()
 """
 
-
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
     """
-    Class with id and name attributes of each state
+    State class:
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship('City', cascade='save-update, merge, delete',
+                          backref='state')
 
